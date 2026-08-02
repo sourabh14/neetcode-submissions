8ba21110e65423a9b0088afbe17b05021a7532fd
Sourabh Shrivastava <simplysourabh_123@yahoo.com>
2026-08-02 10:52:21 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean detectCycle(int u, int n, List<List<Integer>> graph, boolean[] recstack, boolean[] visited) {
+        recstack[u] = true;
+        boolean isCyclic = false;
+        for (int i=0; i<graph.get(u).size(); i++) {
+            int v = graph.get(u).get(i);
+            if (recstack[v]) return true;
+            else {
+                isCyclic = detectCycle(v, n, graph, recstack, visited);
+            }
+            if (isCyclic) break;
+        }
+        recstack[u] = false;
+        visited[u] = true;
+        return isCyclic;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n = numCourses;
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i=0; i<n; i++) graph.add(new ArrayList<>());
+        for (int[] edge: prerequisites) {
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(u).add(v);
+        }
+        boolean[] recstack = new boolean[n];
+        boolean[] visited = new boolean[n];
+
+        for (int i=0; i<n; i++) {
+            if (!visited[i]) {
+                if (detectCycle(i, n, graph, recstack, visited)) return false;
+            }
+        }
+
+        return true;
+    }
+}
